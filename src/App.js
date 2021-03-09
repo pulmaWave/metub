fdad51c858254fecb71f59e41bc53895f0ffd20f
pulmaWave <congthinhq@gmail.com>
2021-03-09 15:32:55 +0700

    Fix layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Layout, Menu } from "antd";
 import {
   HomeOutlined,
-  FireOutlined,
-  MenuUnfoldOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+  BarChartOutlined,
 } from "@ant-design/icons";
 import "./App.scss";
 import logo from "./images/youtube.png";
+import Contents from "./components/homepage/Contents";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -13,82 +15,78 @@ const App = () => {
   return (
     <div>
       <Layout>
-        <Header
-          className="header"
-          style={{ position: "fixed", zIndex: 1, width: "100%" }}
-        >
-          <div className="logo" />
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-            <Menu.Item key="1" className="">
-              <img src={logo} alt="Logo youtube" />
-              Youtube Vanced
+            <Menu.Item key="1">
+              <img src={logo} alt="logo"></img>
+              Youtube advanced
             </Menu.Item>
           </Menu>
         </Header>
-        <Content
+        <Sider
           style={{
-            padding: "0 50px",
-            margin: "24px 16px 0",
-            overflow: "initial",
+            paddingTop: "60px",
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
           }}
         >
-          <Layout
-            className="site-layout-background"
-            style={{ padding: "24px 0" }}
-          >
-            <Sider
+          <div className="logo" />
+          <Menu theme="light" mode="inline" defaultSelectedKeys={["4"]}>
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              Home
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              Trending
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              Subscriptions
+            </Menu.Item>
+            <Menu.Item key="4" icon={<BarChartOutlined />}>
+              Gaming
+            </Menu.Item>
+            <Menu.Item key="5">Music</Menu.Item>
+            <Menu.Item key="6">League of Legends</Menu.Item>
+            <Menu.Item key="7">Movies</Menu.Item>
+            <Menu.Item key="8">Settings</Menu.Item>
+            <Menu.Item key="8">Report</Menu.Item>
+            <Menu.Item key="8">History</Menu.Item>
+            <Menu.Item key="8">Help</Menu.Item>
+            <Menu.Item key="8">Send feedback</Menu.Item>
+            <Menu.Item key="8">Live</Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout" style={{ marginLeft: 200 }}>
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+            <div
               className="site-layout-background"
-              width={200}
-              style={{
-                padding: "20px 0",
-                overflow: "auto",
-                height: "100vh",
-                position: "fixed",
-                left: 0,
-              }}
+              style={{ padding: 24, textAlign: "center" }}
             >
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
-                style={{ height: "100%" }}
-              >
-                <Menu.Item key="sub1" icon={<HomeOutlined />}>
-                  Home
-                </Menu.Item>
-                <Menu.Item key="1">Music</Menu.Item>
-                <Menu.Item key="2">Game</Menu.Item>
-                <Menu.Item key="3">Programing</Menu.Item>
-                <Menu.Item key="4">League of Legend</Menu.Item>
-                <Menu.Item key="sub2" icon={<FireOutlined />}>
-                  Trending
-                </Menu.Item>
-                <Menu.Item key="5">Music</Menu.Item>
-                <Menu.Item key="6">Gaming</Menu.Item>
-                <Menu.Item key="7">Movies</Menu.Item>
-                <Menu.Item key="8">News</Menu.Item>
-                <Menu.Item key="sub3" icon={<MenuUnfoldOutlined />}>
-                  Subscriptions
-                </Menu.Item>
-                <Menu.Item key="5">Music</Menu.Item>
-                <Menu.Item key="6">Gaming</Menu.Item>
-                <Menu.Item key="7">Movies</Menu.Item>
-                <Menu.Item key="8">News</Menu.Item>
-                <Menu.Item key="sub3" icon={<MenuUnfoldOutlined />}>
-                  Subscriptions
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Content
-              style={{ padding: "0 24px", minHeight: 280 }}
-              className="site-layout-content-background"
-            ></Content>
-          </Layout>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Youtube clone ver2021 Created by Neo
-        </Footer>
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+              <Contents />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Bryan
+          </Footer>
+        </Layout>
       </Layout>
+      ,
     </div>
   );
 };
---
+++ src/components/homepage/Contents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Contents.scss"
+
+const Contents = () => {
+  return (
+    <div>   
+      <div className="view">
+
+      </div>
+    </div>
+  )
+}
+
+export default Contents
