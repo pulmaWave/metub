d1d7b7940885af32d25fbd07aec352c738ad59a3
pulmaWave <congthinhq@gmail.com>
2021-03-22 14:05:18 +0700

    Fix layout + route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-
-import Home from "./components/homepage/Home";
+import Home from './page/homepage/Home';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+            <Home />
   );
 }
 
---
+++ src/page/views/View.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./View.scss";
+import picture2 from '../../assets/images/somepic.jpg';
+
+
+const View = () => {
+     
+      return (
+            <div className="view">
+                  <img  src={picture2} alt="background song"/>
+                  
+            </div>
+      )
+}
+
+export default View
