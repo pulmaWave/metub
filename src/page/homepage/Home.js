da55cc8366de2eb5502555967d980bf8d1a31adb
pulmaWave <congthinhq@gmail.com>
2021-03-24 13:09:07 +0700

    Route + background color

--- src/page/homepage/Home.js
+++ src/page/homepage/Home.js
@@ -1,6 +1,6 @@
 import { React, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import View from "../views/View";
 import { Layout, Menu } from "antd";
 import {
@@ -12,9 +12,8 @@ import {
   BarChartOutlined,
 } from "@ant-design/icons";
 import "./Home.scss";
-import picture2 from "../../assets/images/somepic.jpg"
+import picture2 from "../../assets/images/somepic.jpg";
 import logo from "../../assets/images/youtube.png";
-import something from "../../assets/images/something.JPG";
 import Video from "../../components/videos/Video";
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -25,66 +24,72 @@ const Home = () => {
   };
   return (
     <div>
-      <Layout>
-        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-            <Menu.Item key="1">
-              <img src={logo} alt="logo"></img>
-              Youtube advanced
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={setCollapse}
-          style={{
-            paddingTop: "60px",
-            overflow: "auto",
-            height: "100vh",
-            position: "fixed",
-            left: 0,
-          }}
-        >
-          <div className="logo">
-            {/* {(setCollapse ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: toggle(),
-            })} */}
-          </div>
-          <Menu theme="light" mode="inline" defaultSelectedKeys={["4"]}>
+      <Router>
+        <Layout>
+          <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+              <Menu.Item
+                key="1"
+                icon={<MenuFoldOutlined />}
+                onClick={() => toggle()}
+              ></Menu.Item>
+              <Menu.Item key="2">
+                <img src={logo} alt="logo" onClick={() => toggle()}></img>
+                Youtube advanced
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Sider
           
-            <Menu.Item key="1" icon={<HomeOutlined />}>
-              Home
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              Trending
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              Subscriptions
-            </Menu.Item>
-            <Menu.Item key="4" icon={<BarChartOutlined />}>
-              Gaming
-            </Menu.Item>
-            <Menu.Item key="5">Music</Menu.Item>
-            <Menu.Item key="6">League of Legends</Menu.Item>
-            <Menu.Item key="7">Movies</Menu.Item>
-            <Menu.Item key="9">Settings</Menu.Item>
-            <Menu.Item key="10">Report</Menu.Item>
-            <Menu.Item key="11">History</Menu.Item>
-            <Menu.Item key="12">Help</Menu.Item>
-            <Menu.Item key="13">Send feedback</Menu.Item>
-            <Menu.Item key="14">Live</Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout" style={{ marginLeft: 200 }}>
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-            <div
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+            style={{
+              paddingTop: "60px",
+              overflow: "auto",
+              height: "100vh",
+              position: "fixed",
+              left: 0,
+            }}
+          >
+            <Menu theme="light" mode="inline" defaultSelectedKeys={["4"]}>
+              <Menu.Item key="1" icon={<HomeOutlined />}>
+                <Link to="/home">Home</Link>
+              </Menu.Item>
+              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                Trending
+              </Menu.Item>
+              <Menu.Item key="3" icon={<UploadOutlined />}>
+                Subscriptions
+              </Menu.Item>
+              <Menu.Item key="4" icon={<BarChartOutlined />}>
+                Gaming
+              </Menu.Item>
+              <Menu.Item key="5">Music</Menu.Item>
+              <Menu.Item key="6">League of Legends</Menu.Item>
+              <Menu.Item key="7">Movies</Menu.Item>
+              <Menu.Item key="9">Settings</Menu.Item>
+              <Menu.Item key="10">Report</Menu.Item>
+              <Menu.Item key="11">History</Menu.Item>
+              <Menu.Item key="12">Help</Menu.Item>
+              <Menu.Item key="13">Send feedback</Menu.Item>
+              <Menu.Item key="14">Live</Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout" style={{ marginLeft: 200 }}>
+            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Content
               className="site-layout-background"
-              style={{ padding: 24, textAlign: "center" }}
+              style={{
+                margin: "24px 16px",
+                padding: 24,
+                minHeight: 280,
+              }}
             >
-              <Router>
+              <div
+                className="site-layout-background"
+                style={{ padding: 24, textAlign: "center" }}
+              >
                 {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
                 <Switch>
@@ -104,14 +109,14 @@ const Home = () => {
                     <View />
                   </Route>
                 </Switch>
-              </Router>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            Ant Design ©2018 Created by Bryan
-          </Footer>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              Ant Design ©2018 Created by Bryan
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
+      </Router>
     </div>
   );
 };
